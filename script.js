cf256ec33c517c2ba43c6c46ac39fc9336e9c4e3
MrErmita <alessio.bragetti@gmail.com>
2024-10-03 10:49:38 +0200

    fauiìha
    da

---
+++ script.js
@@ -0,0 +1,17 @@
+const MAIL_REGEX = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+const NAME_REGEX = "^[a-zA-Z]+$";
+const PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+//PASSWORD_REGEX needs at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character
+const PHONE_REGEX = "^[0-9]{10}$";
+const CF_REGEX = "^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$" //regex codice fiscale
+
+function checkMail() {
+    let mail = document.getElementById("mailIn").value;
+    console.log(mail);
+    if(!mail.test(MAIL_REGEX)) {
+        alert("Email non valida");
+        document.getElementById("mailIn").style.borderColor = "red";
+    } else {
+        alert("ok");
+    }
+}
